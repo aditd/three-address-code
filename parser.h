025e657d8f110d999bacde2012eb7ad49704b656
Paritosh Panda <paritosh.panda_ug23@ashoka.edu.in>
2022-11-28 17:22:07 +0530

    learn + format

--- parser.h
+++ parser.h
@@ -1,34 +1,34 @@
-#ifndef __PARSER_H 
+#ifndef __PARSER_H
 #define __PARSER_H
-/* Symbol Table Entry */ 
+/* Symbol Table Entry */
 typedef struct symtab {
-    char *name;
-    int value; 
+    char* name;
+    int value;
 } symboltable;
 
-typedef enum { 
+typedef enum {
     PLUS = 1,
-    MINUS, 
+    MINUS,
     MULT,
     MOD,
     DIV
 } opcodeType;
 
-typedef enum { 
+typedef enum {
     ASSIGN = 0,
-    UNARY, 
+    UNARY,
     BINARY
 } operatortype;
 
 typedef struct quad {
     opcodeType op;
     operatortype type;
-    char * result;
-    char * arg1;
-    char * arg2;
+    char* result;
+    char* arg1;
+    char* arg2;
     // ONLY arg1 and result are there if COPY
     // ONLY arg1, op and result are there if UNARY
-    
+
 } quad;
 
 // quad(op=MINUS,type=BIN,$$->name,$1->name,$1->name)
@@ -55,18 +55,18 @@ char print_operator(opcodeType op) {
             return '%';
             break;
     }
-} 
-    
+}
+
 */
 
-/* Look-up Symbol Table */ 
-symboltable *symlook(char *);
-/* Generate temporary variable */ 
-symboltable *gentemp();
+/* Look-up Symbol Table */
+symboltable* symlook(char*);
+/* Generate temporary variable */
+symboltable* gentemp();
 /* Output 3-address codes */
 /* if s3 != 0 ==> Assignment with Binary operator */
 /* if s3 == 0 && c != 0 ==> Assignment with Unary operator */ /* if s3 == 0 && c == 0 ==> Simple Assignment */
-quad emit_bin(char *result, char *arg1,opcodeType operator, char *arg2);
-quad emit_assign(char *result, char *arg1);
-quad emit_un(char *result, char *arg1, opcodeType operator);
+quad emit_bin(char* result, char* arg1, opcodeType operator, char* arg2);
+quad emit_assign(char* result, char* arg1);
+quad emit_un(char* result, char* arg1, opcodeType operator);
 #endif // __PARSER_H
